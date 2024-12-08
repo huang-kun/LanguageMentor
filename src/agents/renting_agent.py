fd76e57c5f707fecc928dd810adb553a8be5b7e4
huang-kun <jack-huang-developer@foxmail.com>
2024-12-08 10:26:02 +0800

    homework: Complete renting scenario.

--- src/agents/renting_agent.py
+++ src/agents/renting_agent.py
@@ -1,9 +1,66 @@
 from .base_scenario_agent import ScenarioAgent
 
+from langchain_ollama.chat_models import ChatOllama  # 导入 ChatOllama 模型
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder  # 导入提示模板相关类
+from langchain_core.messages import HumanMessage  # 导入人类消息类
+from utils.logger import LOG  # 导入日志工具
+
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,  # 基础聊天消息历史类
+    InMemoryChatMessageHistory,  # 内存中的聊天消息历史类
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory  # 导入带有消息历史的可运行类
+
+store = {}
+
+def get_session_history(session_id):
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
 class RentingAgent(ScenarioAgent):
     def __init__(self):
         super().__init__()
         self.name = "Renting Agent"
+
+        prompt_path = "prompts/homework_rent_prompt.txt"
+        with open(prompt_path) as f:
+            self.system_prompt = f.read().strip()
+
+        self.prompt = ChatPromptTemplate.from_messages([
+            ("system", self.system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+        ])
+
+        self.chatbot = self.prompt | ChatOllama(
+            model="llama3.1:8b-instruct-q8_0",
+            max_token=8192,
+            temperature=0.8,
+        )
+
+        self.chatbot_with_history = RunnableWithMessageHistory(
+            self.chatbot,
+            get_session_history,
+        )
+
+        self.config = {
+            "configurable": {
+                "session_id": "renting-house"
+            }
+        }
+
+    def chat(self, user_input):
+        response = self.chatbot.invoke(
+            [HumanMessage(content=user_input)],
+        )
+        return response.content
+    
+    def chat_with_history(self, user_input):
+        response = self.chatbot_with_history.invoke(
+            [HumanMessage(content=user_input)],
+            self.config,
+        )
+        return response.content
 
     def respond(self, user_input):
         # 调用与租房相关的对话逻辑
---
+++ src/homework_prompt.py
@@ -0,0 +1,130 @@
+from openai import OpenAI
+import httpx
+import os
+import textwrap
+
+def create_llm_client():
+    api_key = os.environ["OPENAI_API_KEY"]
+    if "OPENAI_BASE_URL" in os.environ:
+        base_url = os.environ["OPENAI_BASE_URL"]
+        return OpenAI(
+            base_url=base_url, 
+            api_key=api_key,
+            http_client=httpx.Client(
+                base_url=base_url,
+                follow_redirects=True,
+            ),
+        )
+    else:
+        return OpenAI(api_key=api_key)
+
+def get_llm_message(chat_completion):
+    return chat_completion.choices[0].message.content
+
+def test_llm_completion(client):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+        ]
+    )
+    return completion
+
+def test_llm():
+    client = create_llm_client()
+    completion = test_llm_completion(client)
+    print(completion)
+
+def start_llm_conversation(model='gpt-3.5-turbo', system_prompt='You are a helpful assistant.', start_chat=None, output=None, debug=False):
+    client = create_llm_client()
+    messages = [{'role': 'system', 'content': system_prompt}]
+    loop_times = 0
+
+    while True:
+        loop_times += 1
+        if start_chat and loop_times == 1:
+            user_msg = start_chat
+        else:
+            user_msg = input("You: ")
+            if not user_msg:
+                continue
+            if user_msg in ['q', 'quit', 'exit']:
+                break
+
+        messages.append({'role': 'user', 'content': user_msg})
+        chat_comp = client.chat.completions.create(model=model, messages=messages)
+        if debug:
+            print("-------")
+            print(messages)
+            print(chat_comp)
+            print("-------")
+
+        llm_msg = get_llm_message(chat_comp)
+        messages.append({'role': 'assistant', 'content': llm_msg})
+        print(f"AI: {llm_msg}")
+
+    if output:
+        messages.pop(0)
+        conversations = list(map(lambda x: f"{x['role']}: {x['content']}\n", messages))
+        with open(output, 'w') as f:
+            f.writelines(conversations)
+
+def discover_course_goal(requirement_text, output_filename):
+    '''开发英文课程目标和任务的提示'''
+    model = 'gpt-4o-mini'
+
+    project_dir = os.getcwd()
+    find_goal_prompt_path = os.path.join(project_dir, 'prompts', 'discover_goal_prompt_en.txt')
+    
+    with open(find_goal_prompt_path, 'r') as f:
+        system_prompt = f.read().strip()
+
+    start_llm_conversation(
+        model=model,
+        system_prompt=system_prompt,
+        start_chat=requirement_text,
+        output=os.path.join(project_dir, 'temp_works', output_filename)
+    )
+
+def discover_rent_course_goal_homework():
+    # 我想设计一个租房场景的英语课程，请让AI扮演一位房屋中介来指导用户学习英语。
+    requirement_text = "I want to design an English course for a house rental scenario. " \
+    "Please let AI play the role of a real estate agent to guide users to learn English."
+    discover_course_goal(
+        requirement_text=requirement_text,
+        output_filename='rent_course_goal.txt'
+    )
+
+def improve_rent_prompt_homework():
+    """"""
+    model = 'gpt-4o-mini'
+
+    project_dir = os.getcwd()
+    engineer_prompt_path = os.path.join(project_dir, 'prompts', 'prompt_engineer_prompt.txt')
+    old_teacher_prompt_path = os.path.join(project_dir, 'prompts', 'old_conversation_prompt.txt')
+    
+    with open(engineer_prompt_path, 'r') as f:
+        system_prompt = f.read().strip()
+    with open(old_teacher_prompt_path, 'r') as f:
+        teacher_prompt = f.read().strip()
+
+    start_chat = textwrap.dedent('''
+        Please improve the prompt for AI English teacher with few updates:
+        - Generate 3 English example sentences to facilitate the conversation.
+        - Provide a Format that structure reply content of the AI, including teaching review, example sentences, and AI role-played charactor replies.
+        - DO NOT make big changes for original prompt, just make some adjustments to make sure the generated result from this prompt will be stable.
+        ---- Here is the original prompt inside of the dash line area ----\n''')
+    
+    start_chat += teacher_prompt
+    start_chat += "\n-------------------"
+    
+    start_llm_conversation(
+        model=model, 
+        system_prompt=system_prompt,
+        start_chat=None,
+        output=os.path.join(project_dir, 'conversation', 'homework_chat.txt')
+    )
+
+if __name__ == '__main__':
+    discover_rent_course_goal_homework()
